590ffe28ff4b38507231b2b4caea50901be8aa0c
Robert Sterenchak <robertsterenchak@Roberts-MacBook-Pro.local>
2021-04-08 08:19:17 -1000

    Made progress towards displaying 16x16 grid. Used repeat() function within gridTemplateColumns/Areas to make work.

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,38 @@
-console.log('Hello everybody');
+
+
+
+
+const container = document.getElementById('container');
+container.style.backgroundColor = 'white';
+container.style.display = 'grid';
+container.style.gridTemplateColumns = `repeat(16, 1fr)`;
+container.style.gridTemplateRows = `repeat(16, 1fr)`;
+
+const grid_box = document.createElement('div');
+
+grid_box.style.backgroundColor = 'white';
+grid_box.textContent = ('box 1');
+grid_box.style.border = "3px solid black";
+grid_box.style.padding = '5px';
+
+
+
+container.appendChild(grid_box);
+
+function repeatBox(){
+
+    for(let i = 1; i < 17; i++){
+        
+        const (grid_box) + 'i' = document.getElementById('div');// need to dynamically alter name
+
+    }
+} 
+
+
+//try to use grid_template to create a 16 by 16 grid div
+console.dir(grid_box);
+console.dir(container);
+
 
 
 
