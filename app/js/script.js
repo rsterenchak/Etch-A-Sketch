67398a4cfb8d037cf2239bce6c443ab6f630e948
Robert Sterenchak <robertsterenchak@Roberts-MacBook-Pro.local>
2021-04-19 01:45:30 -1000

    Added responsive grid-item height in js, added title, adjusted button grid clear button size

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,5 @@
-
-
-let rows = prompt("How many rows would you like in your grid?"); // rows and cols need to be set via user prompt
-let cols = prompt("How many columns would you like in your grid?");
+let rows = prompt("How many rows and columns would you like in your grid?"); // rows and cols need to be set via user prompt
+let cols = rows;
 
 const topButton = document.getElementById('topButton');
 const container = document.getElementById('container');
@@ -9,42 +7,44 @@ container.style.backgroundColor = 'white';
 
 var btn = document.createElement("BUTTON");
 topButton.appendChild(btn).className = "button-top";
-btn.innerText = "CLEAR THE GRID";
+btn.innerText = "CLEAR GRID";
 
 btn.addEventListener('click', function(){
 
     // let cells = document.getElementsByClassName("grid-items");
 
-    for(let i = 0; i < (rows * cols); i++){
+    // for(let i = 0; i < (rows * cols); i++){
 
-        container.parentNode.removeChild();
+    //     container.parentNode.removeChild();
 
-    }
+    // }
 
 
-    rows = prompt("How many rows would you like in your grid?"); // rows and cols need to be set via user prompt
-    cols = prompt("How many columns would you like in your grid?");
+    // rows = prompt("How many rows would you like in your grid?"); // rows and cols need to be set via user prompt
+    // cols = prompt("How many columns would you like in your grid?");
 
-    repeatBox(rows, cols);
-    addListener(rows, cols);
+    // repeatBox(rows, cols);
+    // addListener(rows, cols);
+
+    location.reload();
 });
 
 
 function repeatBox(rows, cols){
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
+    let boxHeight = (300/(cols));
 
     for(let i = 0; i < (rows * cols); i++){
         
 
         cell = document.createElement("div");
-        cell.innerText = (i + 1);
-
+        cell.innerText = "";
         container.appendChild(cell).className = "grid-items";
         
-
+        cell.style.setProperty('min-height', (boxHeight + "px"));
     }
-
+    
 
 
 } //ends function
@@ -59,7 +59,7 @@ function addListener(rows, cols){
     for(let i = 0; i < (rows * cols); i++){
     
         let gridCell = cells[i].addEventListener("mouseover", func, false);
-        let gridOut = cells[i].addEventListener("mouseout", func1, false);
+        // let gridOut = cells[i].addEventListener("mouseout", func1, false);
 
         function func(){
 
